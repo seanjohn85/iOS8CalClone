482a796c79cf96a3b580f1f42742c172c2d3f359
John <johnkenny@johns-macbook-pro-2.local>
2016-09-19 15:25:08 +0100

    Initial Commit

---
+++ Cal/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cal
+//
+//  Created by JOHN KENNY on 19/09/2016.
+//  Copyright © 2016 JOHN KENNY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
